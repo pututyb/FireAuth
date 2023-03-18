9d397f34e12046e2e73173feabbad87dd3ed98ea
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-03-18 21:02:58 +0700

    Initial Commit

---
+++ FireAuth/FireAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireAuthApp.swift
+//  FireAuth
+//
+//  Created by Putut Yusri Bahtiar on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FireAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
